70e9235b4b8a14b9b46fd6b3238a0c7e92c37bdc
Laura Chicos <12laura@gmail.com>
2020-04-07 22:52:50 -0500

    started ventilator page

--- screens/VentilationScreen.js
+++ screens/VentilationScreen.js
@@ -1,51 +1,37 @@
-import React from 'react';
-import {FlatList, Text, View, Image, TouchableHighlight} from 'react-native';
-import { ventilators } from '../../data/dataArrays';
-import { getNumberOfRecipes } from '../../data/MockDataAPI';
-import React, {Component} from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
 import { Image, Button, Platform, StyleSheet, Text, View, Alert, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
-export default class VentilationScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Ventilators'
-  };
 
-  constructor(props) {
-    super(props);
-  }
-
-  onPressVentilator = item => {
-    const title = item.name;
-    const ventilator = item;
-    this.props.navigation.navigate('RecipesList', { ventilator, title });
-  };
-
-  renderVentilator = ({ item }) => (
-    <TouchableHighlight underlayColor='rgba(73,182,77,1,0.9)' onPress={() => this.onPressVentilator(item)}>
-      <View style={styles.ventilatorsItemContainer}>
-        <Image style={styles.ventilatorsPhoto} source={{ uri: item.photo_url }} />
-        <Text style={styles.ventilatorsName}>{item.name}</Text>
-        <Text style={styles.ventilatorsInfo}>{getNumberOfRecipes(item.id)} recipes</Text>
-      </View>
-    </TouchableHighlight>
+export default function VentilationScreen({navigation}) {
+  return (
+    <View style={styles.ventilatorsItemContainer}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.ventilatorsItemContainer}>
+          <Image
+            source={require('../assets/images/splash.png')}
+            style={styles.ventilatorsPhoto}
+          />
+          <Text style={styles.ventilatorsName}>
+            LTV-1200 Ventilator
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
   );
-
-  render() {
-    return (
-      <View>
-        <FlatList
-          data={ventilators}
-          renderItem={this.renderVentilator}
-          keyExtractor={item => `${item.id}`}
-        />
-      </View>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 0,
+  },
   ventilatorsItemContainer: {
     flex: 1,
     margin: 10,

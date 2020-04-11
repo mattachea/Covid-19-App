38c382f1599de349ffcedb1bac7245586bf92298
Laura Chicos <12laura@gmail.com>
2020-04-11 17:10:29 -0500

    remove Treatment screen?

--- screens/TreatmentScreen.js
+++ screens/TreatmentScreen.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import Accordion from '../components/Accordion'
-
 import { Image, Button, Platform, StyleSheet, Text, View, Alert, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { db } from '../config';
+import { data } from '../App';
+
+
 
 export default class TreatmentScreen extends Component {
   constructor(props) {
@@ -15,11 +16,15 @@ export default class TreatmentScreen extends Component {
   }
 
   async getData() {
-    const snapshot = await db.ref('cpr/Treatment').once('value');
-    let data = snapshot.val();
-    this.setState({ data });
+    try {
+      const snapshot = await db.ref('cpr/epi').once('value');
+      // let data = JSON.stringify(snapshot.val())
+      let data = snapshot.val()
+      this.setState({data})
+    } catch(e) {
+      console.warn(e);
+    }
   }
-
   componentDidMount() {
     this.getData();
   }
@@ -31,55 +36,111 @@ export default class TreatmentScreen extends Component {
           <View style={styles.titleContainer}>
             
             <Image 
-              source={require('../assets/images/treatment.png')}
+              source={require('../assets/images/epidemiology.png')}
               style={styles.titleImage}
             />    
             <Text style={styles.titleText}>
-            Treatment
-            </Text>
-          </View>
-         {/* {this.state.information.map((prop, key) => {
-           return (
-             <Accordion title="hello" data="bitch please" key={key}>{prop[1]}></Accordion>
-           );
-        })} */}
-        <Accordion title="Overall" data={this.state.data.Overall} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        <Accordion title="Medications" data={this.state.data.Medications} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        <Accordion title="Fluids Feeding Renal" data={this.state.data.FluidsFeedingRenal} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        <Accordion title="Respiratory Care" data={this.state.data.RespiratoryCare} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        <Accordion title="Intubation" data={this.state.data.Intubation} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        <Accordion title="Cardiac" data={this.state.data.Cardiac} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        <Accordion title="Discharge" data={this.state.data.Discharge} colorRow = "#E75351" colorChild = "#FFCECE"></Accordion>
-        </ScrollView>
-       </View>
-    )
-  }
-}
+            Epidemiology
+          </Text>
 
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity
+            style={[{backgroundColor: '#7ED551'}, styles.buttonStyle]}
+            onPress={() => this.props.navigation.navigate('DiseaseAgent')}
+          
+          >
+        <Text style={styles.buttonText}>Disease Agent</Text>
+        </TouchableOpacity>
+
+   
+        <TouchableOpacity
+            style={[{backgroundColor: '#7ED551'}, styles.buttonStyle]}
+            onPress={() => this.props.navigation.navigate('Transmission')}
+          >
+        <Text style={styles.buttonText}>Transmission</Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+            style={[{backgroundColor: '#7ED551'}, styles.buttonStyle]}
+            onPress={() => this.props.navigation.navigate('R0')}
+          >
+        <Text style={styles.buttonText}>R0</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+            style={[{backgroundColor: '#7ED551'}, styles.buttonStyle]}
+            onPress={() => this.props.navigation.navigate('CaseFatality')}
+          >
+        <Text style={styles.buttonText}>Case Fatality</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity
+            style={[{backgroundColor: '#7ED551'}, styles.buttonStyle]}
+            onPress={() => this.props.navigation.navigate('RiskFactors')}
+          >
+        <Text style={styles.buttonText}>Risk Factors for Severity</Text>
+        </TouchableOpacity>
+        
+        </View>
+
+</View>
+  </ScrollView>
+    </View>
+   )
+}
+}
+//Styling
 const styles = StyleSheet.create({
   container: {
-   flex:1,
-   backgroundColor: '#ffff',
-
+    flex: 1,
+    backgroundColor: '#fff',
   },
   titleContainer: {
     alignItems: 'center',
     marginTop: 10,
     marginBottom: 20,
-    backgroundColor:'#ffff',
   },
   titleText: {
     marginTop: 30,
     marginBottom: 30,
     fontSize: 36,
-    fontWeight:'bold',
     textAlign: 'center',
-    color: '#E75351',
   },
   titleImage: {
-    width: 100,
-    height: 100,
+    width: 150,
+    height: 150,
     resizeMode: 'contain',
-    marginTop: 3,
+  },
+  buttonContainer: {
+    alignItems:'center',
+  },
+  buttonIcon:{
+    width:50,
+    height:50,
+    resizeMode: 'contain',
+  },
+  buttonStyle: {
+    flexDirection: 'row',
+    marginVertical: 8,
+    alignItems:'center',
+    justifyContent:'center',
+    width:350,
+    height:90,
+    borderRadius:20,
+    shadowColor: 'rgba(0,0,0, .4)', // IOS
+    shadowOffset: { height: 1, width: 0 }, // IOS
+    shadowOpacity: 1, // IOS 
+    shadowRadius: 1, //IOS
+    elevation: 1, // Android
+  },
+  buttonText: {
+    paddingHorizontal: 20,
+    color: '#FFFFFF',
+    fontSize: 24,
+  },
+  contentContainer: {
+    paddingTop: 0,
   },
 });

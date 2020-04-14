be6399cbfc5db5ace445aefb3b87cc7ab6d1f7fe
Devanshu <dsinghjhu2020@gmail.com>
2020-04-13 20:27:34 -0700

    finsihed images

--- components/ContentScreen.js
+++ components/ContentScreen.js
@@ -6,30 +6,41 @@ import { ScrollView } from "react-native-gesture-handler";
 export default function ContentScreen({ route }) {
   const { name } = route.params;
   const { data } = route.params;
-  var array = data.match(/\$(.*?)\$/g);
-  var namePaths = null;
-  if (array !== null) {
-    namePaths = findImageNames(array);
-  }
 
   function createObjects() {
     var objectsList = [];
-    if (namePaths !== null) {
-      namePaths.forEach(namePath => {
-        let text1 = data.split(namePath)[0];
-        let text2 = data.split(namePath)[1];
-        let textObject1 = <Text style = {styles.text}>{text1}</Text>;
-        let textObject2 = <Text style = {styles.text}>{text2}</Text>;
-        objectsList.push(textObject1);
+
+    var indices = [];
+    for(var i=0; i<data.length;i++) {
+      if (data[i] === "$") indices.push(i);
+    }
+
+    if (indices.length == 0) {
+
+      let textObject = <Text key = '1' style = {styles.text}>{data}</Text>;
+      objectsList.push(textObject);
+
+    } else {
+
+      let firstText = data.substring(0, indices[0]);
+      let firstObject = <Text key = '1' style = {styles.text}>{firstText}</Text>;
+      objectsList.push(firstObject);
+
+      for (let i = 0; i < indices.length; i = i + 2) {
+        let imageName = data.substring(indices[i]+1, indices[i+1]);
+        let text = data.substring(indices[i+1]+1, indices[i+2]);
+
         let imageObject = <Image
         style={{ width: 400, height: 400, alignSelf: 'center', margin: 10, transform: [{ scale: 1 }]}}
-        source={{uri: "https://firebasestorage.googleapis.com/v0/b/covid-19-ventilator-training.appspot.com/o/images%2FCPRImages%2F"+namePath+"?alt=media&token=9b535aac-9275-4cfa-b4f3-552010ab594b"}}/>;
+        source={{uri: "https://firebasestorage.googleapis.com/v0/b/covid-19-ventilator-training.appspot.com/o/images%2FCPRImages%2F"+imageName+"?alt=media&token=9b535aac-9275-4cfa-b4f3-552010ab594b"}}/>;
+        
         objectsList.push(imageObject);
-        objectsList.push(textObject2);
-      });
-    } else {
-      let textObject = <Text key='1' style = {styles.text}>{data}</Text>;
-      objectsList.push(textObject);
+
+        let textObject = <Text key = {i} style = {styles.text}>{text}</Text>;
+        objectsList.push(textObject);
+        
+      }
+
     }
     return objectsList;
   }
@@ -46,19 +57,6 @@ export default function ContentScreen({ route }) {
       </ScrollView>
     </View>
   );
-
-  function findImageNames (array) {
-    let imageNames = [];
-    array.forEach(name => {
-      let newName = name.replace('$', '');
-      newName = newName.replace('$', '');
-      console.log(newName);
-      imageNames.push(newName);
-      console.log(imageNames);
-    });
-    console.log(imageNames);
-    return imageNames;
-  }
 
   function fetchCPRImage (namePath) { //fetch any images on this page
     var imgPath = app.storage().ref("/images/CPRImages/"); //google storage route

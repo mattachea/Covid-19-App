a061d58f660abada55776451a521371b580dc320
Matthew Chea <mattachea@gmail.com>
2020-04-15 02:26:00 -0400

    formatting the content

--- components/ContentScreen.js
+++ components/ContentScreen.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState }  from "react";
-import {StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-
+import CheckBox from "react-native-check-box";
 
 export default function ContentScreen({ route }) {
   const { name } = route.params;
@@ -9,61 +9,104 @@ export default function ContentScreen({ route }) {
 
   function createObjects() {
     var objectsList = [];
-
     var indices = [];
     for(var i=0; i<data.length;i++) {
       if (data[i] === "$") indices.push(i);
     }
-
     if (indices.length == 0) {
-
       let textObject = <Text key = '1' style = {styles.text}>{data}</Text>;
       objectsList.push(textObject);
-
     } else {
-
       let firstText = data.substring(0, indices[0]);
       let firstObject = <Text key = '1' style = {styles.text}>{firstText}</Text>;
       objectsList.push(firstObject);
-
       for (let i = 0; i < indices.length; i = i + 2) {
         let imageName = data.substring(indices[i]+1, indices[i+1]);
         let text = data.substring(indices[i+1]+1, indices[i+2]);
-
         let imageObject = <Image
-        style={{ width: 400, height: 400, alignSelf: 'center', margin: 10, transform: [{ scale: 1 }]}}
+        style={{width: 350, height: 350, alignSelf: 'center', margin: 10, resizeMode: 'contain'}}
         source={{uri: "https://firebasestorage.googleapis.com/v0/b/covid-19-ventilator-training.appspot.com/o/images%2FCPRImages%2F"+imageName+"?alt=media&token=9b535aac-9275-4cfa-b4f3-552010ab594b"}}/>;
-        
         objectsList.push(imageObject);
-
         let textObject = <Text key = {i} style = {styles.text}>{text}</Text>;
         objectsList.push(textObject);
-        
       }
-
     }
     return objectsList;
+
+  // // code works but need to change database to the correct structure.
+  //   let k = 0 //for unique keys to get rid of warnings
+
+  //   function bulletPoint(value, padding) {
+  //     const list = value.content.split("~");
+  //         list.map((item) => {
+  //           console.log(k)
+  //           componentsList.push(
+  //             <View key = {k++} style={{ flexDirection: "row", padding: padding }}>
+  //               <Text style={styles.text}>{"\u2022"}</Text>
+  //               <Text style={[{ flex: 1, paddingLeft: 5 }, styles.text]}>
+  //                 {item}
+  //               </Text>
+  //             </View>
+  //           );
+  //         });
+  //   }
+
+  //   var componentsList = [];
+  //   Object.entries(data).map(([key, value]) => {
+  //     switch (value.type) {
+  //       case "text":
+  //         componentsList.push(
+  //           <Text
+  //             key={k++}
+  //             style={[{ fontWeight: value.fontWeight }, styles.text]}
+  //           >
+  //             {value.content + "\n"}
+  //           </Text>
+  //         );
+  //         break;
+  //       case "mainBullet":
+  //         bulletPoint(value, 0);
+  //         break;
+  //       case "subBullet":
+  //         bulletPoint(value, 20);
+  //         break;
+  //       case "mainCheckbox":
+  //         bulletPoint(value, 0) //need to actually implement checkboxes later
+  //         break;
+  //       case "subCheckbox":
+  //         bulletPoint(value, 20) //need to actually implement checkboxes later
+  //         break;
+  //       case "image":
+  //         console.log(value);
+  //         componentsList.push(
+  //           <View key={k++} style={styles.contentContainer}>
+  //             <Image
+  //               style={styles.image}
+  //               source={{
+  //                 uri:
+  //                   "https://firebasestorage.googleapis.com/v0/b/covid-19-ventilator-training.appspot.com/o/images%2FCPRImages%2F" +
+  //                   value.content +
+  //                   "?alt=media&token=9b535aac-9275-4cfa-b4f3-552010ab594b",
+  //               }}
+  //             />
+  //           </View>
+  //         );
+  //         break;
+  //     }
+  //   });
+  //   return componentsList;
   }
 
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container}>
         <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>
-            {name}
-          </Text>
-          {createObjects()}
+          <Text style={styles.titleText}>{name}</Text>
         </View>
+        <View style={styles.contentContainer}>{createObjects()}</View>
       </ScrollView>
     </View>
   );
-
-  function fetchCPRImage (namePath) { //fetch any images on this page
-    var imgPath = app.storage().ref("/images/CPRImages/"); //google storage route
-    var ventRef = imgPath.child(namePath);//add name of image file
-    ventRef.getDownloadURL().then(function(url) {
-    return url;
-  });}
 }
 
 //Styling
@@ -74,24 +117,25 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20,
-    padding: 20
+    margin: 20,
   },
   titleText: {
-    marginBottom: 30,
     fontSize: 36,
     textAlign: "center",
-    fontFamily:"Avenir-roman",
+    fontFamily: "Avenir-roman",
   },
-  titleImage: {
-    width: 150,
-    height: 150,
-    resizeMode: "contain",
+  image: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    aspectRatio: 1,
   },
   text: {
-      fontSize: 18,
-      fontFamily:"Avenir-roman",
-
-  }
+    fontSize: 18,
+    // fontFamily: "Avenir-roman",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 10,
+  },
 });
--- screens/ClinicianScreen.js
+++ screens/ClinicianScreen.js
@@ -4,21 +4,6 @@ import { ScrollView } from "react-native-gesture-handler";
 import MyButton from "../components/MyButton";
 import { db } from "../config";
 
-// 3 'Airway_Management': "#1082C7", 
-// 5 'Cardiopulmonary_Arrest': "#C73110",
-// 6 'Cardiovascular_Support': "#C73110",
-// 12 'Discharge': "#38B406", 
-// 8 'ECMO': "#C73110",
-// 1 'Epidemiology': "#EFCB34", 
-// 7 'Hematology_and_Coagulation': "#C73110",
-// 10'Imaging': "#38B406",
-// 2 'Pharmacological_Treatment': "#EFCB34",
-// 9 'Recognition': "EFCB34",
-// 11 'Renal_Support': "#38B406", 
-// 4 'Respiratory_Support': "#1082C7",
-
-// };
-
 export default function ClinicianScreen({ navigation }) {
   const [cprData, setData] = useState({});
 
@@ -27,8 +12,6 @@ export default function ClinicianScreen({ navigation }) {
       const snapshot = await db.ref("categories/").once("value");
       let data = snapshot.val();
       setData(data);
-
-      //console.log(data)
     } catch (e) {
       console.warn(e);
     }
@@ -45,12 +28,6 @@ export default function ClinicianScreen({ navigation }) {
         <MyButton name={name} color={value.color} data={key} key={key} content = 'Buttonscreen' />
       
     });
-    // buttonList = [ //custom button order
-    //   buttonList[5], buttonList[8], //yellow
-    //   buttonList[6], buttonList[2], buttonList[1], buttonList[4], //red
-    //   buttonList[0], buttonList[11], buttonList[10], //blue
-    //   buttonList[7], buttonList[9], buttonList[3] //green
-    // ];
     return buttonList;
   }
 

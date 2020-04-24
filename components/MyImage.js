61dc9ec5c6efdd9840b0579af55b70550399aa1c
Matthew Chea <mattachea@gmail.com>
2020-04-24 12:56:35 -0400

    undo image modal, doesn't work well in web

--- components/MyImage.js
+++ components/MyImage.js
@@ -1,24 +1,40 @@
 import React from "react";
-import { Dimensions, StyleSheet, View} from "react-native";
+import { Image, Dimensions, StyleSheet, View, Modal } from "react-native";
+import ImageZoom from 'react-native-image-pan-zoom';
 import ImageModal from "react-native-image-modal";
+import { Props } from "react-native-image-pan-zoom/built/image-zoom/image-zoom.type";
 
+let windowHeight = Dimensions.get("window").height;
 let windowWidth = Dimensions.get("window").width;
 export default function MyImage(props) {
   return (
     <View styles={styles.container}>
-      <ImageModal
-        resizeMode="contain"
-        imageBackgroundColor={props.backgroundColor}
-        style={{
-          width: windowWidth-50,
-          height: 350,
-        }}
-        source={{
-          uri:
-            props.uri,
-        }}
-      />
+      <ImageZoom cropWidth={windowWidth-50}
+        cropHeight={windowHeight-50}
+        imageWidth={windowWidth-50}
+        imageHeight={windowHeight-50}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: props.uri,
+          }}
+        /> 
+      </ImageZoom>
     </View>
+  //   <View styles={styles.container}>
+  //   <ImageModal
+  //     resizeMode="contain"
+  //     imageBackgroundColor={props.backgroundColor}
+  //     style={{
+  //       width: windowWidth-50,
+  //       height: 350,
+  //     }}
+  //     source={{
+  //       uri:
+  //         props.uri,
+  //     }}
+  //   />
+  // </View>
   );
 }
 
@@ -26,9 +42,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+
   },
   image: {
     flex: 1,
+    // width: windowWidth,
+    // height: undefined,
+    // aspectRatio: 1,
+    // margin: 20,
+    // alignItems: 'center',
     resizeMode: "contain",
   },
 });

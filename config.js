1075405453cc62788583d7ef982f2f539e00cfac
Devanshu <dsinghjhu2020@gmail.com>
2020-04-29 13:02:06 -0700

    updated config.js with new firebase project

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNeeXQOz2-S3sk5bP_clXvLd3iLnPYeOg",
-  authDomain: "covid-19-ventilator-training.firebaseapp.com",
-  databaseURL: "https://covid-19-ventilator-training.firebaseio.com",
-  projectId: "covid-19-ventilator-training",
-  storageBucket: "covid-19-ventilator-training.appspot.com",
-  messagingSenderId: "25658178614",
-  appId: "1:25658178614:web:0b1acea69309b56e2e6c97",
-  measurementId: "G-X4TNFM04Y9"
+  apiKey: "AIzaSyAS257cb6HUCS2UmxOUOjEGeFttAxcXnVE",
+  authDomain: "covid-reference.firebaseapp.com",
+  databaseURL: "https://covid-reference.firebaseio.com",
+  projectId: "covid-reference",
+  storageBucket: "covid-reference.appspot.com",
+  messagingSenderId: "275615521425",
+  appId: "1:275615521425:web:08f50c7983945c70a3bd30",
+  measurementId: "G-FM96JGK28C"
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
